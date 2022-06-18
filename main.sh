c042b50e76c38988416bbca971578e1e7a6d47b6
Anastasia Slabucho <101647250+an-sla@users.noreply.github.com>
2022-06-18 21:54:46 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+ls -lah
+mkdir CV
+cd CV
+ls -lah
 xelatex -interaction=nonstopmode -halt-on-error main.tex
+
+ls -lah
+cd ..
+ls -lah
+echo startcopy
+cp CV/resume.pdf resume.pdf
+ls -lah
 
 rm main.out main.log main.aux
 exit
