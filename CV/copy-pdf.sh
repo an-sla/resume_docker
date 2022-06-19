ffaba88239d4ecc1899583f42c57b7b869ddaadf
Anastasia Slabucho <101647250+an-sla@users.noreply.github.com>
2022-06-19 09:48:36 +0200

    Update copy-pdf.sh

--- CV/copy-pdf.sh
+++ CV/copy-pdf.sh
@@ -1,9 +1,5 @@
-ls -lah
 git config --global user.email "anastasia.slabucho@gmail.com"
 git config --global user.name "Anastasia Slabucho"
 cp main.pdf resume.pdf
-ls -lah
 cd ..
-ls -lah
 cd ..
-ls -lah
