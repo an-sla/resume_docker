0dbb3d3ac1496a76be47be81e863c948734bf48b
Anastasia Slabucho <101647250+an-sla@users.noreply.github.com>
2022-06-19 09:48:10 +0200

    Update main.sh

--- CV/main.sh
+++ CV/main.sh
@@ -1,12 +1,7 @@
-ls -lah
 cd CV
-ls -lah
 xelatex -interaction=nonstopmode -halt-on-error main.tex
-ls -lah
 cd ..
-ls -lah
 echo startcopy
 cp CV/main.pdf resume.pdf
-ls -lah
 git config --global user.email "anastasia.slabucho@gmail.com"
 git config --global user.name "Anastasia Slabucho"
